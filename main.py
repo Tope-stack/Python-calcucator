fdb2dc59c39dcd504de2cb7b59a7253fb2fbee89
Yusuff Tope <>
2022-05-20 15:40:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+number_1 = float(input('Enter your first number'))
+number_2 = float(input('Enter your second number'))
+
+#addition
+print('{} + {} = '.format(number_1, number_2))
+print(number_1 + number_2)
+
+#subtraction
+print('{} - {} = '.format(number_1, number_2))
+print(number_1 - number_2)
+
+#multiplication
+print('{} * {} = '.format(number_1, number_2))
+print(number_1 * number_2)
+
+#division
+print('{} / {} = '.format(number_1, number_2))
+print(number_1 / number_2)
+
+#modulus operator
+print('{} % {} = '.format(number_1, number_2))
+print(number_1 % number_2)
